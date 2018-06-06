170705cfe975e6f8635946ac61d17f9030a78ff3
Marceliwut <mmirzynski@gmail.com>
2018-06-06 13:56:42 +0200

    Initial commit
    PreferedNetworkChange -initial commit
    
    Contains:
    PreferedNetworkChange.ps1
    Set-NICto52GHzBandwidth.ps1
    Set-NICto52GHzBandwidth.xml
    
    PreferedNetworkChange.ps1:
    Copies files to clients from .\list.txt file. Registers XML file as scheduled task on machines.
    
    Set-NICto52GHzBandwidth.ps1
    Changes Wi-Fi Prefered Bandwith to work on 5.x Ghz. Saves log in C:\Temp\Set-NICto52GhzBandwidth_$ComputerName.log file.
    
    Set-NICto52GHzBandwidth.xml
    Contains information about Task Scheduler Task that runs script above when user locks the computer.

---
+++ PreferedNetworkChange.ps1
@@ -0,0 +1,32 @@
+﻿$progress = 0
+$LogFile = ".\logs\log_"  + $(Get-Date -Format "dd") + "-" + $(Get-Date -Format "MM") + "-" + $(Get-Date -Format "yyyy") + "_" + $(Get-Date -Format "HH") + $(Get-Date -Format "mm") + ".log"
+$PreferredBand = "3. Prefer 5GHz band", "3 - Prefer 5Ghz Band", "3 - Prefer 5.2Ghz Band", "3. Prefer 5.2GHz band"
+if(Test-Path .\RemoveList.txt){
+    Write-Host "List not found"    
+}
+
+else {
+foreach($line in Get-Content .\list.txt){
+    $progress++
+    }
+    Write-Output "Performing prefered network change for: " $progress " computers"
+	foreach($line in Get-Content .\list.txt){
+	#test if client is online
+		if(Test-Connection $line -Count 1){
+			Write-Host $line "online. Starting the process"
+			#copying files to client
+			robocopy .\Set-NICto52GHzBandwidth \\$line\c$\temp\Set-NICto52GHzBandwidth\ /e /z /R:1 /W:2
+			if(Test-Path \\$line\c$\temp\Set-NICto52GHzBandwidth){
+				Write-Output "File transfer completed"
+				Invoke-Command -ComputerName $line -ScriptBlock { Register-ScheduledTask -Xml (get-content 'C:\temp\Set-NICto52GHzBandwidth\Set-NICto52GHzBandwidth.xml' | out-string) -TaskName "Set-NICto52GHzBandwidth" –Force }
+				Write-Output "$line ok" | Out-File $LogFile -Append				
+			}
+			else {
+				Write-Output "File transfer to $line failed" | Out-File $LogFile -Append
+			}
+		}
+		else {
+		Write-Output "$line offline" | Out-File $LogFile -Append
+		}
+	}
+ }
